8831c65cd4527fa29c683df3bd4956592e07d027
Alisa Kovaliova <aliko875@student.liu.se>
2018-09-18 11:42:17 +0200

    första fil

---
+++ lab2.py
@@ -0,0 +1,36 @@
+import socket
+import os
+
+##serverPort = 8000
+##webPage = "www.liu.se"
+##
+##serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+##print("Server socket created:")
+##print(serverSocket)
+###serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+##serverSocket.bind((socket.gethostname(), serverPort))
+##print("Server socket with hostname",socket.gethostname(),"is bound to",serverPort)
+##serverSocket.listen(1)
+##
+##clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+##print("Client socket is created:")
+##print(clientSocket)
+##clientSocket.connect((webPage, 80))
+##print("Client socket is connected to",webPage)
+##
+##while 1:
+##    (clientSocket, clientAddress) = serverSocket.accept()
+##    print("Have received a connection from",clientSocket,",",clientAddress)
+##    child = os.fork()
+    
+class serverSocket:
+
+    def __init__(self, port_server):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Server socket is created")
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((socket.gethostname(), port_server))
+        print("Socket is bound to",port_server)
+        self.sock.listen(10)
+    
+
