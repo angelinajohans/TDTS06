d2af0941bc8dda0c6e51a6d9027f671f9b2a0b63
Alisa Kovaliova <aliko875@student.liu.se>
2018-09-20 08:09:22 +0200

    städat :D och utökat. Client-sidan påbörjad, men ej komplett

--- lab2.py
+++ lab2.py
@@ -1,42 +1,49 @@
 import socket
 import os
-
-##serverPort = 8000
-##webPage = "www.liu.se"
-##
-##serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-##print("Server socket created:")
-##print(serverSocket)
-###serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-##serverSocket.bind((socket.gethostname(), serverPort))
-##print("Server socket with hostname",socket.gethostname(),"is bound to",serverPort)
-##serverSocket.listen(1)
-##
-##clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-##print("Client socket is created:")
-##print(clientSocket)
-##clientSocket.connect((webPage, 80))
-##print("Client socket is connected to",webPage)
-##
-##while 1:
-##    (clientSocket, clientAddress) = serverSocket.accept()
-##    print("Have received a connection from",clientSocket,",",clientAddress)
-##    child = os.fork()
     
 class serverSocket:
-
+    
     def __init__(self, port_server):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("Server socket is created")
+        print("Server socket created")
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((socket.gethostname(), port_server))
-        print("Socket is bound to",port_server)
+        print(socket.gethostname())
+        print("Socket is bound to port",port_server)
         self.sock.listen(10)
+        print("Server is listening")
+
+    def hear_client(self):
+        (clientSocket, clientAddress) = self.sock.accept()
+        print("Connection established")
+        child = os.fork()
+        self.sock.close()
+        return(clientSocket)
+
+    def recv_from_client(self, clientSocket):
+        msg = clientSocket.recv(6000)
+        print(msg)
 
 
+    def close_socket(self):
+        self.sock.close()
+    
+class clientSocket:
+
+    def __init__(self, webpage):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Client socket created")
+        self.sock.connect((webpage, 80))
+        print("Server socket connects to", webpage, "on port 80")
+
+    
 def main():
-    servsock = serverSocket(8000)
-
+    serverSock = serverSocket(8080)
+    while 1:
+        clientSock = serverSock.hear_client()
+##        print(procRunning.clientSocket)
+##        print(procRunning.clientAddress)
+        serverSock.recv_from_client(clientSock)
 
 if __name__ == "__main__":
-    main()
+   main()
