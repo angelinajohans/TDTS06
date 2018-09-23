eb25aa7036bb891435af607d62e5b8f622f5c07c
Alisa Kovaliova <aliko875@student.liu.se>
2018-09-23 15:34:13 +0200

    The line with Proxy-Connection: keep-alive is exchanged to Connection: close. Uncomment the adjacent lines to just remove the connection information in header. Complaining about the severname provided to the sock.connect function. If providing IP address the forwarding almost work :)

--- lab2.py
+++ lab2.py
@@ -1,8 +1,28 @@
 import socket
 import threading
 import re
-import re
-    
+
+class clientSocket:
+
+    def __init__(self, webpage):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Client socket created")
+        print("Trying to connect to",webpage)
+        print(type(webpage))
+        self.sock.connect((webpage, 80))
+        print("Server socket connects to", webpage, "on port 80")
+
+    def forward_request(self, request):
+        print("Entered forward_request")
+        request_to_send = request.encode("utf=8", errors="strict")
+        print(type(request_to_send))
+        self.sock.send(request_to_send)
+        webserver_msg = self.sock.recv(4000)
+        return webserver_msg
+
+    def close_socket(self):
+        self.sock.close()
+
 class serverSocket:
     
     def __init__(self, port_server):
@@ -22,48 +42,44 @@ class serverSocket:
         print("New thread created")
         print(new_thread_id)
         new_thread_id.start()
-        #self.sock.close()
-        #print(os.getpid(), "closed socket")
+
+    def handle_request(self, incommingSocket, request, webpage):
+        print("Want to create client socket and connect to",webpage[0])
+        clientSock = clientSocket(webpage[0])
+        print(clientSock)
+        print(type(request))
+        webserver_msg = clientSock.forward_request(request)
+        incommingSocket.send(webserver_msg)
+        clientSocket.close_socket()
 
     def recv_from_client(self, incommingSocket):
         bytes_request = incommingSocket.recv(4000)
+        print(type(bytes_request))
         string_request = bytes_request.decode(encoding="utf-8", errors="strict")
         print('Original string:', string_request)
         url_of_page = re.findall('Host:(.+)', bytes_request.decode('utf-8'))
         new_request = re.sub('https?:\/+\/.+ ', '/ ', string_request)
-        print('The url of the page:', url_of_page[0])
-        print('New request1', new_request)
+        new_request_to_send = re.sub('[Pp]roxy.[Cc]onnection:.+','Connection: close\r', new_request)
+        #divide_msg = re.search('Proxy(.+)', new_request)
+        #req_no_connKeepAlive = new_request[:divide_msg.start()] + new_request[divide_msg.end():]
+        #new_ready_to_send = re.sub('\n\n', '\n', req_no_connKeepAlive)
+        print('The url of the page:',url_of_page[0])
+        print('New request1\n',new_request)
+        #print('Request without Connection: Keep-Alive \n',req_no_connKeepAlive)
+        print('Request with no Keel-Alive and one new line \n',new_request_to_send)
+        self.handle_request(incommingSocket, new_request_to_send, url_of_page)
+
 
 
 # Don't know if this will ever be used...
     def close_socket(self):
         self.sock.close()
-    
-class clientSocket:
 
-    def __init__(self, webpage):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("Client socket created")
-        self.sock.connect((webpage, 80))
-        print("Server socket connects to", webpage, "on port 80")
-
-class searcher_researcher:
-
-    def find_url(self, recv_msg):
-        position_url = recv_mssg.find("://")
-        if position_url == -1:
-            position_url = recv_msg.find("Host:")
-            url_end = recv_msg[(position_url + 6):].find("\r\n")
-            url = recv_msg[(position_url + 6):-((len(recv_msg) - (url_end) - 1))]
-        else:
-            url_end = recv_msg[(position_url + 3):].find(" ")
-            url = recv_msg[(position_url + 3):-((len(recv_msg) - (url_end) - 1))]
     
 def main():
-    serverSock = serverSocket(8000)
+    serverSock = serverSocket(8080)
     while 1:
         serverSock.hear_client()
-        print("Next print is the msg")
 
 
 if __name__ == "__main__":
