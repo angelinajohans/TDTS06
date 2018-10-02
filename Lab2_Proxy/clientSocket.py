77cab6f3b07cb7d3d8e384a470c6f40e4b9cb0dd
Alisa Kovaliova <aliko875@student.liu.se>
2018-10-02 13:20:28 +0200

    nu funkar inget, har lagt till loopar

--- Lab2_Proxy/clientSocket.py
+++ Lab2_Proxy/clientSocket.py
@@ -8,30 +8,45 @@ class clientSocket:
     def __init__(self, webpage):
         #webpage = webpage[:len(webpage)-1]
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("Client socket created")
-        print("Trying to connect to",webpage)                
+        print'Client socket created'
+        print'Trying to connect to',webpage                
         #try:
         self.sock.connect((webpage,80))
-        print("Server socket connects to",webpage,"on port 80")
+        print'Server socket connects to',webpage,'on port 80'
         #except socket.gaierror:
-        #    print("The socket could not be properly connected!")
+        #    print('The socket could not be properly connected!')
             
 
     #Forwards the request from server side to the webserver and
     #receives the response message from the webserver.
     #The received response is returned
     def forward_request(self, request):
-        print("Entered forward_request")
-        request_to_send = request.encode("utf=8", errors="strict")
+        recv_msg = [b'']
+        send_msg_size = 0
+        sent_msg_size = 0
+        print'Entered forward_request'
+        request_to_send = request.encode('utf=8', errors='strict')
+        send_msg_size = len(request_to_send)
+        print'This is the send_msg_size:',send_msg_size
+        print'This is the sent_msg_size:',sent_msg_size
         print(type(request_to_send))
-        self.sock.send(request_to_send)
-        print("Request to the webserver has been sent\n",request_to_send)
-        webserver_msg = self.sock.recv(4096)
-        print("A response message has been received")
-        return webserver_msg
+        while sent_msg_size < send_msg_size:
+            sent_msg = self.sock.sendall(request_to_send[sent_msg_size:])
+            print'This is the sent_msg:',sent_msg
+            if sent_msg != 0:
+                sent_msg_size = sent_msg_size[0] + sent_msg
+                print'This is the sent_msg_size:',sent_msg_size,'after the plus operation'
+        print'Request to the webserver has been sent\n',request_to_send
+        recv_part_msg = self.sock.recv(4096)
+        print'This is the revc_part_msg:',recv_part_msg
+        while recv_part_msg != 0:
+            recv_msg[0] = recv_msg[0]+recv_part_msg
+            recv_part_msg = self.sock.recv(4096)
+        print'A response message has been received'
+        return recv_msg
 
     #Closes the client socket
     def close_socket(self):
-        print("About to close client socket")
+        print'About to close client socket'
         self.sock.close()
-        print("Client socket is closed")
+        print'Client socket is closed'
--- Lab2_Proxy/uglyWordsFinder.py
+++ Lab2_Proxy/uglyWordsFinder.py
@@ -10,8 +10,8 @@ class uglyWordsFinder:
     #Contains image returs False
     #Does not contain image returns True
     def need_to_investigate(self, request): #or just url?
-        print("In the need_to_investigate function")
-        pic_formats = ["tif","tiff","bmp","jpg","jpeg","gif","png","eps"]
+        print'In the need_to_investigate function'
+        pic_formats = ['tif','tiff','bmp','jpg','jpeg','gif','png','eps']
         
         #For every item in string list pic_formats
         #look for the format keyword in the input string
@@ -21,10 +21,10 @@ class uglyWordsFinder:
             #If the image format keyword is not found, continue with next keyword
             #else (if the keyeord is found) return True; needs to be investagated
             if exist != -1:
-                print("The object does not need to be investigated")
+                print'The object does not need to be investigated'
                 continue
             else:
-                print("The object needs to be investigated")
+                print'The object needs to be investigated'
                 return True
         return False
         
@@ -32,7 +32,7 @@ class uglyWordsFinder:
     #If contains fobidden words, it is unacceptable and returns False
     #If does not contain forbidden words, it is acceptable and returns True
     def acceptable(self, obj_of_interest):
-        forbidden_words = ["[Bb]ritney ?[Ss]pears", "[Pp]aris ?[Hh]ilton", "[Ll]inkoping", "[Ss]ponge[Bb]ob"]
+        forbidden_words = ['[Bb]ritney ?[Ss]pears', '[Pp]aris ?[Hh]ilton', '[Ll]inkoping', '[Ss]ponge[Bb]ob']
         
         #If the object of interest needs to be investigated
         #enter search for forbidden words
@@ -45,15 +45,15 @@ class uglyWordsFinder:
             for i in forbidden_words:
                 found_words = re.findall(i,obj_of_interest)
                 if found_words != []:
-                    print("The object is dirty")
-                    print("This is what the function searched through:\n",obj_of_interest)
+                    print'The object is dirty'
+                    print'This is what the function searched through:\n',obj_of_interest
                     return False
                 else:
                     continue
-            print("The object is clean")
-            print("This is what the function searched through:\n",obj_of_interest)
+            print'The object is clean'
+            print'This is what the function searched through:\n',obj_of_interest
             return True
         else:
-            print("The object is clean")
-            print("This is what the function searched through:\n",obj_of_interest)
+            print'The object is clean'
+            print'This is what the function searched through:\n',obj_of_interest
             return True
